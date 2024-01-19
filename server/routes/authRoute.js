68291f2719ef5ae59cd40c28b2dd21ee57fde831
Eric Acosta <72636775+ericacosta505@users.noreply.github.com>
2024-01-19 14:03:06 -0700

    Home route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
 import AuthHandlers from "../handlers/authHandler.js";
+import userVerification from "../middlewares/authMiddleware.js";
 
 const { Signup, Login } = AuthHandlers;
 
 const router = express.Router();
 
+router.post("/", userVerification);
 router.post("/signup", Signup);
 router.post("/login", Login);
 
