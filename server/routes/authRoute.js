d1c12149f82f998359576ed313ef6cd9fdf2e806
Eric Acosta <72636775+ericacosta505@users.noreply.github.com>
2024-01-19 13:37:03 -0700

    Login route

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,8 +1,11 @@
-import Signup from "../handlers/authHandler.js";
 import express from "express";
+import AuthHandlers from "../handlers/authHandler.js";
+
+const { Signup, Login } = AuthHandlers;
 
 const router = express.Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
 
 export default router;
