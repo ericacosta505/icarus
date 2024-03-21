109136e37f3d78384f2624fde736b19768d6a4f2
Eric Acosta <72636775+ericacosta505@users.noreply.github.com>
2024-03-21 11:14:28 -0700

    Update AddEntryForm.jsx

--- client/src/Components/AddEntryForm.jsx
+++ client/src/Components/AddEntryForm.jsx
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 const AddEntryForm = ({ isDarkMode }) => {
+  const [mealName, setMealName] = useState("");
+  const [proteinAmount, setProteinAmount] = useState("");
+
+  const handleAddEntry = () => {};
+
   return (
     <div
       className={`${isDarkMode ? "darkContainerLong" : "addEntryContainer"}`}
     >
       <div className="containerTitle">Add Entry</div>
       <div className="entryRow">
-        <input placeholder="Meal Name"></input>
-        <input placeholder="Protein Amount"></input>
-        <button>Add</button>
+        <input
+          placeholder="Meal Name"
+          onChange={(e) => {
+            setMealName(e.target.value);
+          }}
+          value={mealName}
+        ></input>
+        <input
+          placeholder="Protein Amount"
+          onChange={(e) => {
+            setProteinAmount(e.target.value);
+          }}
+          value={proteinAmount}
+        ></input>
+        <button onClick={handleAddEntry}>Add</button>
       </div>
     </div>
   );
