ee309b11c99b3bc76cba34ce436e5a9828c7614c
Eric Acosta <72636775+ericacosta505@users.noreply.github.com>
2024-05-02 09:22:57 -0700

    Start delete entry feature

--- client/src/Components/EntryList.jsx
+++ client/src/Components/EntryList.jsx
@@ -4,21 +4,20 @@ const EntryList = ({ isDarkMode, todaysEntries, isEntryLoading }) => {
   return (
     <div className={`${isDarkMode ? "darkContainer" : "entryListContainer"}`}>
       Today's Entries
-
-      {isEntryLoading ?(<Loader /> ): (todaysEntries.length === 0 ? (
+      {isEntryLoading ? (
+        <Loader />
+      ) : todaysEntries.length === 0 ? (
         <div>No Entries Found</div>
       ) : (
         <ul>
           {todaysEntries.map((entry) => (
-            <li key={entry._id}>
+            <li key={entry._id} className="todayEntryItems">
               {entry.mealName} - Protein: {entry.proteinAmount}g
+              <p className="deleteEntry">X</p>
             </li>
           ))}
         </ul>
-      ))}
-
-
-      
+      )}
     </div>
   );
 };
